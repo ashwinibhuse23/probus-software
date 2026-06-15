cb4dafee86dc99b72424668b8b566071bc093364
ashwinibhuse23 <bhuseashwini864@gmail.com>
2026-06-15 18:27:38 +0530

    Updated project files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import CommonButton from './CommonButton';
-import logo from '../assets/Images/Probus-software.png';
+import logo from '../assets/Images/Probussoftware.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
